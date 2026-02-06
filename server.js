692d96d2dab4e85b5c53c2458f9080c5ab3e2384
Rajat Thakur <rajat.thakur@nxtgenvirtue.com>
2026-02-07 00:47:26 +0530

    development

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const pool = require('./db');
+const pool = require('./db.js');
 const app = express();
 const PORT = 3001;
 
